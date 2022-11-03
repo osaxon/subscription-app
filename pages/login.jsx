e25a8dcc3a6725e98d540f16a04b05fa653b2578
Oli <oliverrsaxon@gmail.com>
2022-11-03 20:31:26 +0000

    updated login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -4,47 +4,27 @@ import Image from "next/future/image";
 import Layout from "../components/Layout";
 
 const Login = ({ providers }) => {
-	const { data: session } = useSession();
-
-	if (session) {
-		return (
-			<div>
-				<p>Welcome, {session.user.name}</p>
-				<button onClick={() => signOut()} className="btn btn-primary">
-					Sign out
-				</button>
-				<Image
-					className="rounded-full"
-					src={session.user.image}
-					width={38}
-					height={38}
-					alt="Your avatar"
-				/>
-			</div>
-		);
-	} else {
-		return (
-			<Layout>
-				<div className="layout h-screen flex flex-col justify-center items-center content-center">
-					<div className="flex flex-col items-center gap-4">
-						{/* Render out all OAuth Providers for user to log in. Redirect to dashboard after successful log in. OAuth option prompts for log in every time. */}
-						{Object.values(providers).map((provider) => (
-							<div key={provider.name}>
-								<button
-									className="link"
-									onClick={() =>
-										signIn(provider.id, { prompt: "login" })
-									}
-								>
-									Sign in with {provider.name}
-								</button>
-							</div>
-						))}
-					</div>
+	return (
+		<Layout>
+			<div className="layout h-screen flex flex-col justify-center items-center content-center">
+				<div className="flex flex-col items-center gap-4">
+					{/* Render out all OAuth Providers for user to log in. Redirect to dashboard after successful log in. OAuth option prompts for log in every time. */}
+					{Object.values(providers).map((provider) => (
+						<div key={provider.name}>
+							<button
+								className="link"
+								onClick={() =>
+									signIn(provider.id, { prompt: "login" })
+								}
+							>
+								Sign in with {provider.name}
+							</button>
+						</div>
+					))}
 				</div>
-			</Layout>
-		);
-	}
+			</div>
+		</Layout>
+	);
 };
 
 export async function getServerSideProps(context) {
